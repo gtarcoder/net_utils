04d770e09738ffc9c6f72e13d11b3cc8bc791844
Jason Sun <skc361@163.com>
2015-10-14 22:19:45 +0800

    modify utils.h utils.cpp @ 2015-10-14

--- udp_socket.cpp
+++ udp_socket.cpp
@@ -26,7 +26,7 @@ int UdpSocket::SendTo(char* buf, int len, const char* dst_ip, uint16_t dst_port)
     return result;
 }
 int UdpSocket::SendTo(char* buf, int len, const struct sockaddr_in& sock_addr){
-    int result = sendto(socket_fd_, buf, len, 0, (sockaddr*)&sock_addr, sizeof(sockaddr));
+     int result = sendto(socket_fd_, buf, len, 0, (sockaddr*)&sock_addr, sizeof(sockaddr));
     if (result < 0){
         perror("udp send\n");
     }
@@ -37,7 +37,7 @@ int UdpSocket::RecvFrom(char* buf, int buf_len, uint32_t* from_ip, uint16_t* fro
     struct sockaddr_in sock_addr;
     socklen_t addr_len = sizeof(sock_addr);
     int recv_bytes = recvfrom(socket_fd_, buf, buf_len, 0, (sockaddr*)&sock_addr, &addr_len);
-    if (recv_bytes < 0){
+    if (recv_bytes <= 0){
         perror("udp recv\n");
     }
     if (from_ip){
@@ -49,6 +49,17 @@ int UdpSocket::RecvFrom(char* buf, int buf_len, uint32_t* from_ip, uint16_t* fro
 
     return recv_bytes;
 }
+
+int UdpSocket::RecvFrom(char* buf, int buf_len, struct sockaddr_in* sock_addr){
+    socklen_t addr_len = sizeof(struct sockaddr_in);
+    int recv_bytes = recvfrom(socket_fd_, buf, buf_len, 0, (sockaddr*)sock_addr, &addr_len);
+    if (recv_bytes <= 0){
+        perror("udp recv\n");
+    }
+    return recv_bytes;
+}
+
+
 int UdpSocket::WaitMsg(int time_out, int *elapsed_time){
     const int MSG_IDLE = 0, MSG_ARRIVAL = 1;
 	fd_set read_fds;
--- udp_socket.h
+++ udp_socket.h
@@ -8,10 +8,12 @@ public:
     ~UdpSocket();
 
     bool Create();
+    bool Connect(uint32_t peer_ip, uint16_t peer_port);
 
     int SendTo(char* buf, int len, const char* dst_ip, uint16_t dst_port); 
     int SendTo(char* buf, int len, const struct sockaddr_in& target_addr); 
     int RecvFrom(char* buf, int buf_len, uint32_t* from_ip, uint16_t* from_port);
+    int RecvFrom(char* buf, int buf_len, struct sockaddr_in* addr);
     int WaitMsg(int time_out, int* elapsed_time);
 };
 #endif
